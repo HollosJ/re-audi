0e00be35b73dc77b471fc8171b61d70b158590e5
HollosJ <hollosj99@gmail.com>
2020-06-21 21:07:15 +0100

    Updates

---
+++ precache-manifest.86c635436443ad19596701b1420b75a2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f36ba42f4a1fe99cc90489091dbd2c8",
+    "url": "/re-audi/index.html"
+  },
+  {
+    "revision": "be7f2cb0bd202a9f134d",
+    "url": "/re-audi/static/css/main.b307ee96.chunk.css"
+  },
+  {
+    "revision": "6d619a73ea1b9bc8a30c",
+    "url": "/re-audi/static/js/2.c8f29a0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/re-audi/static/js/2.c8f29a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7f2cb0bd202a9f134d",
+    "url": "/re-audi/static/js/main.8ceb90a2.chunk.js"
+  },
+  {
+    "revision": "c40f076ac367ea220214",
+    "url": "/re-audi/static/js/runtime-main.cb53f980.js"
+  },
+  {
+    "revision": "89a3ae0732dc8a8aa8f4e8b96a9f7740",
+    "url": "/re-audi/static/media/audilogo.89a3ae07.svg"
+  },
+  {
+    "revision": "89d5a246a4bc9f0316d27c95b53d1edc",
+    "url": "/re-audi/static/media/hero-poster.89d5a246.jpg"
+  },
+  {
+    "revision": "c616b02fc20736545c035f4f7ae79f64",
+    "url": "/re-audi/static/media/herovid.c616b02f.mp4"
+  },
+  {
+    "revision": "21dee7d3590a1a5f54529747417844c4",
+    "url": "/re-audi/static/media/nav-covid.21dee7d3.jpg"
+  },
+  {
+    "revision": "ee49d5ee0227f9cf954abe65647faf09",
+    "url": "/re-audi/static/media/nav-tilegrey.ee49d5ee.jpg"
+  },
+  {
+    "revision": "4f0a2a9092487276a7b6a42ff83b258d",
+    "url": "/re-audi/static/media/tile-about.4f0a2a90.jpg"
+  },
+  {
+    "revision": "5ee32df14ba3f50bceb7ddad6a7f1d43",
+    "url": "/re-audi/static/media/tile-discover.5ee32df1.jpg"
+  },
+  {
+    "revision": "aee50e03ba67b33bdabf46e42f3b3731",
+    "url": "/re-audi/static/media/tile-electric.aee50e03.jpg"
+  },
+  {
+    "revision": "25a1168dcf9871a3a8467fd2c42bd1cf",
+    "url": "/re-audi/static/media/tile-fleet.25a1168d.jpg"
+  },
+  {
+    "revision": "ee46d623d75acb4fb1061ac2d9146e91",
+    "url": "/re-audi/static/media/tile-models.ee46d623.jpg"
+  },
+  {
+    "revision": "67d17cf3f615406dd5823b3f9b8c8a48",
+    "url": "/re-audi/static/media/tile-used.67d17cf3.jpg"
+  }
+]);
